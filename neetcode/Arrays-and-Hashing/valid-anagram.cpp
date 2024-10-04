7409a83b453e8a395f1044428baa1cf9ccdfc628
thehashimq <hashimq905@gmail.com>
2024-10-04 08:14:21 +0530

    solved 3rd problem

--- neetcode/Arrays-and-Hashing/valid-anagram.cpp
+++ neetcode/Arrays-and-Hashing/valid-anagram.cpp
@@ -1,33 +1,22 @@
 #include <algorithm>
 #include <iostream>
 
-bool isAnagram(std::string s, std::string t);
-
-int main() {
-  std::string s = "racecar";
-  std::string t = "carrtce";
-
-  if (isAnagram(s, t)) {
-    std::cout << "Strings " << s << " " << t << " are anagram" << std::endl;
-  } else {
-    std::cout << "String " << s << " " << t << " are not anagram" << std::endl;
-  }
-  return 0;
-}
-
-bool isAnagram(std::string s, std::string t) {
-  if (s.size() != t.size()) {
-    return false;
-  }
-  std::sort(s.begin(), s.end());
-  std::sort(t.begin(), t.end());
-
-  for (int i = 0; i < s.size(); i++) {
-    if (s[i] != t[i]) {
+class Solution {
+public:
+  bool isAnagram(std::string s, std::string t) {
+    if (s.size() != t.size()) {
       return false;
-    } else {
-      continue;
     }
+    std::sort(s.begin(), s.end());
+    std::sort(t.begin(), t.end());
+
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] != t[i]) {
+        return false;
+      } else {
+        continue;
+      }
+    }
+    return true;
   }
-  return true;
-}
+};
