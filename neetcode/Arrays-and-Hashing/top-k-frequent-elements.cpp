96d08a3fa6ef770bea25ee4ccb5eeaedb2d0f623
thehashimq <hashimq905@gmail.com>
2024-12-12 11:30:39 +0530

    first neetcode medium done

---
+++ neetcode/Arrays-and-Hashing/top-k-frequent-elements.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  std::vector<int> topKFrequent(std::vector<int> &nums, int k) {
+    std::vector<int> num;
+    std::unordered_map<int, int> list;
+
+    for (const auto &v : nums) {
+      if (list.find(v) != list.end()) {
+        list[v] += 1;
+      } else {
+        list[v] = 1;
+      }
+    }
+
+    for (const auto &v : list) {
+      std::cout << v.first << " " << v.second << '\n';
+    }
+    while (k > 0) {
+      int max = getMaxValue(list);
+      num.push_back(max);
+      list[max] = -1;
+      --k;
+    }
+    return num;
+  }
+
+  int getMaxValue(std::unordered_map<int, int> &frequencyCount) {
+    int maxValue{};
+    int maxKey{};
+    for (auto it : frequencyCount) {
+      if (it.second > maxValue) {
+        maxKey = it.first;
+        maxValue = it.second;
+      }
+    }
+    std::cout << "Max = " << maxKey << " " << maxValue << '\n';
+    return maxKey;
+  }
+};
+
+int main() {
+  std::vector<int> nums = {1, 1, 1, 2, 2, 3};
+  Solution s;
+  std::vector<int> out = s.topKFrequent(nums, 2);
+  for (const auto &v : out) {
+    std::cout << v << ' ';
+  }
+}
